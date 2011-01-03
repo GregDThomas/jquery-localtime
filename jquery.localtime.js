9d24c9c9fe565daa71d39ffac22638cbf2fceed8
greg.d.thomas@gmail.com <greg.d.thomas@gmail.com@1202477c-5160-5f7f-87b0-a54b413a1f71>
2011-01-03 15:10:01 +0000

    Fix issue 2; clear up the JSLint issues
    git-svn-id: https://jquery-localtime.googlecode.com/svn/trunk@17 1202477c-5160-5f7f-87b0-a54b413a1f71

--- jquery.localtime.js
+++ jquery.localtime.js
@@ -3,27 +3,30 @@
  *
  * Copyright (c) 2010 Greg Thomas
  */
+ 
+ /* Note; the following two lines imform JSLint - http://www.jslint.com/ - that this is supposed
+  *	to run within a browser, and that the jQuery variable is defined elsewhere 
+  */
+  
+ /*jslint browser: true */
+ /*global jQuery: true */
 (function ($) {
 	$.localtime = ( function () {
 
 		var _format = "yyyy-MM-dd HH:mm:ss";
-	
-		var logException = function( exception ) {
-			setTimeout( "throw( '" + exception.replace(/'/g, "\\'") + "');", 0 );
-		};
-		
+
 		var getUnsignedInteger = function( stringToParse ) {
-			if( stringToParse.toString().search(/^[0-9]+$/) != 0) {
+			if( stringToParse.toString().search(/^[0-9]+$/) !== 0) {
 				throw new Error( "'" + stringToParse + "' is not an integer" );
 			}
 			return parseInt( stringToParse, 10 );
 		};
-	
-		var longMonths = ['January','February','March'
-							, 'April','May','June'
-							, 'July','August','September'
-							, 'October','November','December'];
-	
+
+		var longMonths = ['January','February','March',
+							'April','May','June',
+							'July','August','September',
+							'October','November','December'];
+
 		var getUTCFields = function( objDate ) {
 			var year = objDate.getUTCFullYear();
 			var month = objDate.getUTCMonth() + 1;
@@ -33,8 +36,8 @@
 			var second = objDate.getUTCSeconds();
 			var millisecond = objDate.getUTCMilliseconds();
 			return [year, month, date, hour, minute, second, millisecond, 0];
-		}
-	
+		};
+
 		var getLocalFields = function( objDate ) {
 			var year = objDate.getFullYear();
 			var month = objDate.getMonth() + 1;
@@ -45,18 +48,10 @@
 			var millisecond = objDate.getMilliseconds();
 			var tzOffset = objDate.getTimezoneOffset();
 			return [year, month, date, hour, minute, second, millisecond, tzOffset];
-		}
-	
-		var formatUTCDateTime = function( dateToFormat, timeFormat ) {
-			return formatDateTime( getUTCFields( dateToFormat), timeFormat );
 		};
-		
-		var formatLocalDateTime = function( dateToFormat, timeFormat ) {
-			return formatDateTime( getLocalFields( dateToFormat, timeFormat ) );			
-		};
-	
+
 		var formatDateTime = function( dateFields, timeFormat ) {
-		
+
 			var year = dateFields[0].toString();
 			var month = dateFields[1].toString();
 			var date = dateFields[2].toString();
@@ -67,13 +62,13 @@
 			var tzOffset = dateFields[7];
 			var tzSign = (tzOffset < 0) ? "-" : "+";
 			tzOffset = Math.abs(tzOffset);
-			
+
 			// Parse the format string, one char at a time
-			if( timeFormat == undefined ) {
+			if( timeFormat === undefined ) {
 				timeFormat = _format;
-			}			
+			}
 			var formattedDate = "", pattern = "";
-			for( i = 0;  i < timeFormat.length; i ++ ) {
+			for( var i = 0;  i < timeFormat.length; i ++ ) {
 				pattern += timeFormat.charAt( i );
 				// Have we reached the end of the pattern?
 				if( i == timeFormat.length-1 || timeFormat.charAt( i ) != timeFormat.charAt( i + 1 ) ) {
@@ -96,13 +91,13 @@
 						case "S": formattedDate += millisecond; break;
 						case "SS": formattedDate += ("0"+millisecond).slice(-2); break;
 						case "SSS": formattedDate += ("00"+millisecond).slice(-3); break;
-						case "z": formattedDate += tzSign + parseInt( tzOffset / 60 ); break;
-						case "zz": formattedDate += tzSign + ("0"+parseInt( tzOffset / 60 )).slice(-2); break;
-						case "zzz": formattedDate += tzSign 
-														+ ("0"+parseInt( tzOffset / 60 )).slice(-2)
-														+ ":"
-														+ ("0"+ tzOffset % 60 ).slice(-2)
-														; break;
+						case "z": formattedDate += tzSign + parseInt( tzOffset / 60, 10 ); break;
+						case "zz": formattedDate += tzSign + ("0"+parseInt( tzOffset / 60, 10 )).slice(-2); break;
+						case "zzz": formattedDate += tzSign +
+														("0"+parseInt( tzOffset / 60, 10 )).slice(-2) +
+														":" +
+														("0"+ tzOffset % 60 ).slice(-2); 
+														break;
 						default: formattedDate += pattern;
 					}
 					// Reset the pattern
@@ -110,19 +105,27 @@
 				}
 			}
 			return formattedDate;
-			
+
 		};
-	
+
+		var formatUTCDateTime = function( dateToFormat, timeFormat ) {
+			return formatDateTime( getUTCFields( dateToFormat), timeFormat );
+		};
+
+		var formatLocalDateTime = function( dateToFormat, timeFormat ) {
+			return formatDateTime( getLocalFields( dateToFormat, timeFormat ) );
+		};
+
 		return {
-		
+
 			setFormat: function( format ) {
 				_format = format;
 			},
-			
+
 			parseISOTimeString: function( isoTimeString ) {
 				isoTimeString = isoTimeString.toString();
 				// Are we using UTC or local time? UTC ends in "Z"
-				var isUTC = isoTimeString.charAt( isoTimeString.length - 1 ) == "Z"
+				var isUTC = isoTimeString.charAt( isoTimeString.length - 1 ) == "Z";
 				// Strip the trailing Z, if necessary
 				if( isUTC ) {
 					isoTimeString = isoTimeString.substr( 0, isoTimeString.length - 1 );
@@ -132,7 +135,10 @@
 				switch( isoTimeString.length ) {
 					case 23:
 						hasMilliseconds = true;
+						hasSeconds = true;
+						break;
 					case 19:
+						hasMilliseconds = false;
 						hasSeconds = true;
 						break;
 					case 16:
@@ -142,7 +148,7 @@
 					default:
 						throw new Error( isoTimeString + " is not a supported date/time string" );
 				}
-									
+
 				var year = getUnsignedInteger( isoTimeString.substr( 0, 4 ) );
 				var month = getUnsignedInteger( isoTimeString.substr( 5, 2 ) );
 				if( month > 12 ) { throw new Error(month + " is not a valid month"); }
@@ -155,7 +161,7 @@
 				var second = ( hasSeconds ? getUnsignedInteger( isoTimeString.substr( 17, 2 ) ) : 0 );
 				if( second > 59 ) { throw new Error(second + " is not a valid second"); }
 				var millisecond = ( hasMilliseconds ? getUnsignedInteger( isoTimeString.substr( 20, 3 ) ) : 0 );
-				
+
 				var objDate = new Date( 2000, 0, 15);
 				if( isUTC ) {
 					objDate.setUTCFullYear( year );
@@ -174,28 +180,28 @@
 					objDate.setSeconds( second );
 					objDate.setMilliseconds( millisecond );
 				}
-				
+
 				return objDate;
 			},
-			
+
 			toUTCTime: function( timeString, timeFormat ) {
 				return formatUTCDateTime( $.localtime.parseISOTimeString( timeString ), timeFormat );
-				
+
 			},
-		
+
 			toLocalTime: function( timeString, timeFormat ) {
 				return formatLocalDateTime( $.localtime.parseISOTimeString( timeString ), timeFormat );
 			}
-		}
+		};
 	}());
 }(jQuery));
 
-$(document).ready(function () {
-    $(".localtime").each(function (idx, elem) {
-        if ($(elem).is(":input")) {
-            $(elem).val($.localtime.toLocalTime($(elem).val()));
+jQuery(document).ready(function () {
+    jQuery(".localtime").each(function (idx, elem) {
+        if (jQuery(elem).is(":input")) {
+            jQuery(elem).val(jQuery.localtime.toLocalTime(jQuery(elem).val()));
         } else {
-            $(elem).text($.localtime.toLocalTime($(elem).text()));
+            jQuery(elem).text(jQuery.localtime.toLocalTime(jQuery(elem).text()));
         }
     });
 });
