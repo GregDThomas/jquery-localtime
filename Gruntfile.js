ef63fe90b02f00597826510fd1d98aaba98e4724
GregDThomas <greg.d.thomas@gmail.com>
2013-02-21 11:38:29 +0000

    Set the TZ so the tests pass wherever you are

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,6 +12,11 @@ module.exports = function(grunt) {
       '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
       ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
     // Task configuration.
+	env: {
+		setTZ : {
+			TZ : 'Europe/London'
+		}
+	},
     clean: {
       files: ['dist']
     },
@@ -74,14 +79,14 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-env');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'qunit', 'clean', 'concat', 'uglify']);
+  // Default task.  
+  grunt.registerTask('default', ['env:setTZ', 'jshint', 'qunit', 'clean', 'concat', 'uglify']);
 
 };
